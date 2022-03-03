e9a17fea4eb4c724b5f85296d67ee60d74e0ea84
Kevin Roney <ktroney.pdx@gmail.com>
2022-03-03 11:46:16 -0800

    added DOM elements to app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // import functions
 
 // grab DOM elements
-
+const templat = document.querySelector('#template');
+const selectEl = document.querySelector('select');
+const list = document.querySelector('#list');
 // set event listeners 
     // get user input
     // use user input to update state 
